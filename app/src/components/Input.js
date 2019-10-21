3c3da04cac3e9336a26d2057f2c7d219a0ff33c2
Wojtek <drlech@gmail.com>
2019-10-21 14:40:34 +0200

    Add a todo app

---
+++ app/src/components/Input.js
@@ -0,0 +1,36 @@
+import React, { useState, useCallback } from 'react';
+import styled from '@emotion/styled';
+import PropTypes from 'prop-types';
+
+const Wrapper = styled.div`
+  display: flex;
+`;
+
+const StyledInput = styled.input`
+  flex-grow: 1;
+  margin-right: 10px;
+`;
+
+export const Input = ({ onAdd }) => {
+  const [text, setText] = useState('');
+
+  const handleChange = useCallback(e => {
+    setText(e.target.value);
+  }, []);
+
+  const handleAdd = useCallback(() => {
+    onAdd(text);
+    setText('');
+  }, [text]);
+
+  return (
+    <Wrapper>
+      <StyledInput value={text} onChange={handleChange} />
+      <button onClick={handleAdd}>Add!</button>
+    </Wrapper>
+  );
+};
+
+Input.propTypes = {
+  onAdd: PropTypes.func.isRequired
+};
